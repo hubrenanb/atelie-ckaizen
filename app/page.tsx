21af15c0f9ad1e1ff792fc13c62e5bfee36aa349
hubrenanb <hub.renanb@gmail.com>
2025-12-16 20:39:50 -0300

    Criando a hero section inicial da home

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <section className="flex min-h-[60vh] items-center justify-center">
-      <h2 className="text-4xl font-sans">
-        Bem-vindo ao Ateliê CKaizen
-      </h2>
-    </section>
+ <section className="min-h-[70vh] flex items-center">
+  <div className="mx-auto max-w-6x1 px-6">
+
+    <h1 className="text-5x1 font-serif leading-tight">
+      Ateliê CKaizen
+    </h1>
+
+    <p className="mt-6 max-w-xl text-lg leading-relaxed">
+      Criação artesanal com propósito, onde cada detalhe
+      representa cuidado, processo e evolução contínua.
+    </p>
+
+  </div>
+ </section>
   );
 }

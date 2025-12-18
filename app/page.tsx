c8cc14aec8544e204bb7b6748ce6232c835c7b27
hubrenanb <hub.renanb@gmail.com>
2025-12-18 20:12:23 -0300

    Correção de Bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,34 @@
-
 import Image from "next/image";
+import Section from "@/components/Section";
 
-export default function Home () {
+export default function Home() {
   return (
-    <section className="min-h-[70vh] flex items-center">
-      <div className="mx-auto max-w-6xl px-6 grid gap-12 md:grid-cols-2 items-center">
-        
-        {/* Texto */}
+    <Section className="grid gap-12 md:grid-cols-2 items-center min-h-[70vh]">
+      
+      {/* Texto */}
       <div>
-        <h1 className="text-5xl font-serif leading-relaxed">
+        <h1 className="text-5xl font-serif leading-tight">
           Ateliê CKaizen
         </h1>
 
-        <p className="mt-6 max-w=xl text-lg leading relaxed">
-        No Ateliê CKaizen, cada peça é criada com atenção aos detalhes, 
-        respeito ao processo e dedicação à costura artesanal
+        <p className="mt-6 max-w-xl text-lg leading-relaxed">
+          No Ateliê CKaizen, cada peça é criada com atenção aos detalhes,
+          respeito ao processo e dedicação à costura artesanal.
         </p>
       </div>
 
       {/* Imagem */}
       <div className="w-full">
         <Image
-        src="/images/tecido.jpg"
-        alt="Detalhe de tecido e costura artesanal"
-        width={600}
-        height={400}
-        className="rounded-lg object-cover"
-        priority
+          src="/images/tecido.jpg"
+          alt="Detalhe de tecido e costura artesanal"
+          width={600}
+          height={400}
+          className="rounded-lg object-cover"
+          priority
         />
       </div>
 
-      </div>
-    </section>
+    </Section>
   );
 }
--- components/Section.tsx
+++ components/Section.tsx
@@ -1,20 +1,14 @@
 import { ReactNode } from "react";
 
 interface SectionProps {
-    children: ReactNode;
-    className?: string;
+  children: ReactNode;
+  className?: string;
 }
 
-export default function Section ({
-    children,
-    className = "",
-}: SectionProps) {
-    return (
-        <section className={`bg-[#F5EDE3] text-[#4A3F3A] py-24 ${className}`}>
-            <div className="mx-auto max-w-6xl px-6">
-                {children}
-            </div>
-        </section>        
-
-    );
+export default function Section({ children, className = "" }: SectionProps) {
+  return (
+    <section className={`mx-auto max-w-6xl px-6 py-24 ${className}`}>
+      {children}
+    </section>
+  );
 }

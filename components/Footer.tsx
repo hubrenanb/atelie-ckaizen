eb994428a507d6350713c048dcd94c0697465706
hubrenanb <hub.renanb@gmail.com>
2025-12-18 20:31:09 -0300

    Correção de BUG

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer () {
     return (
         <footer className="w-full border-t border-black/10 mt-16">
-            <div className="mx-auto max-w-6x1 px-6 py-6 text-sm">
+            <div className="mx-auto max-w-6xl px-6 py-6 text-sm">
               © {new Date().getFullYear()} Ateliê CKaizen
             </div>
         </footer>

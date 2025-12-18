d37d36bc823558d261d3b69dc5f193d2335a22bb
hubrenanb <hub.renanb@gmail.com>
2025-12-18 18:57:05 -0300

    Atualização BG Mobile 3

--- components/Section.tsx
+++ components/Section.tsx
@@ -10,7 +10,7 @@ export default function Section ({
     className = "",
 }: SectionProps) {
     return (
-        <section className={`bg-[#F5EDE3] py-24 ${className}`}>
+        <section className={`bg-[#F5EDE3] text-[#4A3F3A] py-24 ${className}`}>
             <div className="mx-auto max-w-6xl px-6">
                 {children}
             </div>
